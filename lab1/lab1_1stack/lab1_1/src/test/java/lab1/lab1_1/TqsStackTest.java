a262cf41dee27dfbfd2e5ba4dbe6010e19bc8afa
Alexandre <alexandrepp07@ua.pt>
2022-03-12 17:43:04 +0000

    file updates

--- lab1/lab1_1stack/lab1_1/src/test/java/lab1/lab1_1/TqsStackTest.java
+++ lab1/lab1_1stack/lab1_1/src/test/java/lab1/lab1_1/TqsStackTest.java
@@ -1,5 +1,22 @@
 package lab1.lab1_1;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class TqsStackTest {
-    
+    private TqsStack<String> stackTest;
+
+    @BeforeEach
+    void set_up(){
+        stackTest = new TqsStack<String>();
+    }
+
+    @DisplayName("checking if a stack is empty")
+    @Test
+    public void isEmpty(){
+        assertTrue(stackTest.isEmpty());
+    }
 }
