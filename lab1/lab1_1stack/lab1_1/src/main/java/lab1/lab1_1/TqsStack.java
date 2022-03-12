c46c2d130ec0c9a749c43ad732676beaac85f5f7
Alexandre <alexandrepp07@ua.pt>
2022-03-12 19:08:38 +0000

    lab1_1 stack is completed

--- lab1/lab1_1stack/lab1_1/src/main/java/lab1/lab1_1/TqsStack.java
+++ lab1/lab1_1stack/lab1_1/src/main/java/lab1/lab1_1/TqsStack.java
@@ -5,24 +5,42 @@ import java.util.NoSuchElementException;
 
 public class TqsStack<T> {
     private LinkedList<T> stack;
+    private int maxBound = -1;
 
     public TqsStack(){
         this.stack = new LinkedList<T>();
     }
 
-    // add an item on the top
-    public void push(T x){
-
+    public TqsStack(int i){
+        this.stack = new LinkedList<T>();
+        this.maxBound = i;
     }
 
-    // remove the item at the top
-    public void pop(){
+    // add an item on the top
+    public void push(T x){
+        if (maxBound==-1){
+            stack.push(x);
+        } else{
+            if (this.size()<this.maxBound){
+                stack.push(x);
+            } else{
+                throw new IllegalStateException();
+            }
+        }
         
     }
 
+    // remove the item at the top
+    public T pop() throws NoSuchElementException{
+        return (T) stack.pop();
+    }
+
     // return the item at the top (without removing it)
-    public T peek(){
-        return "";
+    public T peek() throws NoSuchElementException{
+        if (stack.peekFirst()!=null){
+            return (T) stack.peekFirst();
+        }
+        throw new NoSuchElementException();
     }
 
     // return the number of items in the stack
--- lab1/lab1_1stack/lab1_1/src/test/java/lab1/lab1_1/TqsStackTest.java
+++ lab1/lab1_1stack/lab1_1/src/test/java/lab1/lab1_1/TqsStackTest.java
@@ -1,10 +1,15 @@
 package lab1.lab1_1;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
 
 public class TqsStackTest {
     private TqsStack<String> stackTest;
@@ -18,5 +23,77 @@ public class TqsStackTest {
     @Test
     public void isEmpty(){
         assertTrue(stackTest.isEmpty());
+    }
+
+    @DisplayName("size zero on construction")
+    @Test
+    public void sizeZeroOnConstruction(){
+        assertEquals(0, stackTest.size());
+    }
+
+    @DisplayName("after n pushes, stack is not empty and size equals n")
+    @Test
+    public void afterPushes(){
+        stackTest.push("Alex");
+        stackTest.push("João");
+        stackTest.push("Rui");
+        assertFalse(stackTest.isEmpty());
+        assertEquals(3, stackTest.size());
+    }
+
+    @DisplayName("after push n, pops n")
+    @Test
+    public void popAfterPush(){
+        stackTest.push("PUSH-POP");
+        assertEquals("PUSH-POP", stackTest.pop());
+    }
+
+    @DisplayName("one pushes x then peeks, the value returned is x, but the size stays the same")
+    @Test
+    public void peekAfterPush(){
+        stackTest.push("PUSH-PEEK");
+        int size = stackTest.size();
+        assertEquals("PUSH-PEEK", stackTest.peek());
+        assertEquals(size, stackTest.size());
+    }
+
+    
+    @DisplayName("if the size is n, then after n pops, the stack is empty and has a size 0")
+    @Test
+    public void popSize(){
+        int size = stackTest.size();
+        for (int i = 0; i < size; i++){
+            stackTest.pop();
+        }
+        assertTrue(stackTest.isEmpty());
+        assertEquals(0, stackTest.size());
+    }
+
+    @DisplayName("popping from an empty stack does throw a NoSuchElementException")
+    @Test
+    public void popEmpty() {
+        assertThrows( NoSuchElementException.class,()->{stackTest.pop();});
+    }
+
+    @DisplayName("peeking into an empty stack does throw a NoSuchElementException")
+    @Test
+    public void peekEmpty(){
+        assertThrows(NoSuchElementException.class,()->{stackTest.peek();});
+    }
+
+    @DisplayName("push into a full bounded stack")
+    @Test
+    public void pushBounded() {
+
+        TqsStack<Integer> newStack = new TqsStack<Integer>(3);
+
+        newStack.push(1);
+        newStack.push(2);
+        newStack.push(3);
+
+        assertThrows(IllegalStateException.class, () -> {
+            newStack.push(999);
+        });
+
     }
 }
